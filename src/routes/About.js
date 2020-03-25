724c5d87bc747c6d9d8f3f9c19f7c38247775021
ofkande <imddande@gmail.com>
2020-03-25 22:46:20 +0900

    initial files

---
+++ src/routes/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function About() {
+    return (
+        <div className="container about__container">
+            <h2>Let's Movie!</h2>
+            <p>Welcome to my Movie site! <br />
+                This site has some information about movies. <br />
+                Let's enjoy it! </p>
+            <small>@ofkande</small>
+        </div>
+    )
+
+}
+
+export default About;
